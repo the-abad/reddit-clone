6cd00655592182de5432062dc0a0ad6567de52c2
Zeeshan Abad Siddiqui <46226684+the-abad@users.noreply.github.com>
2023-09-17 11:11:27 +0530

    Update ModalWrapper.tsx

--- src/components/Modal/ModalWrapper.tsx
+++ src/components/Modal/ModalWrapper.tsx
@@ -14,6 +14,7 @@ import React from "react";
 type ModalWrapperProps = {
   isOpen: boolean;
   onClose: () => void;
+  children: React.ReactNode;
 };
 
 const ModalWrapper: React.FC<ModalWrapperProps> = ({
